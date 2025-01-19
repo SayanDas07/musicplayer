23695662d64629487e4c8139a146836af6d17b27
Sayan Das <sayandasraja56@gmail.com>
2025-01-19 17:17:03 +0530

    update CORS origin to production URL

--- backend/index.js
+++ backend/index.js
@@ -13,7 +13,7 @@ const PORT = process.env.PORT || 5000;
 
 app.use(cors(
     {
-        origin: 'http://localhost:5173',
+        origin: 'https://musicplayer-gamma-rosy.vercel.app/',
         credentials: true
     }
 ));
